15caf7f5e740bc3c470fb7ab03174a827d9760e3
Magnus Larsson <maglars2@gmail.com>
2023-10-27 14:58:31 +0200

    Refactor to proper python

--- backend/backend.py
+++ backend/backend.py
@@ -7,88 +7,96 @@ import xml.etree.ElementTree
 
 DEFAULT_GPX_PATH = "../data"
 
-def getGpxPath():
+
+def get_gpx_path():
     if len(sys.argv) == 1:
         return DEFAULT_GPX_PATH
     else:
         return sys.argv[1]
-    
-def getGpxFiles():
-    path = getGpxPath()
+
+
+def get_gpx_files():
+    path = get_gpx_path()
     return [abspath(join(path, f)) for f in listdir(path) if isfile(join(path, f)) and f.endswith(".gpx")]
 
-def parseGpx(filename):
+
+def parse_gpx(filename):
     print("Parsing " + filename)
-    
+
     template = string.Template("{http://www.topografix.com/GPX/1/1}$tag")
-    trkptTag = template.substitute(tag="trkpt")
-    timeTag = template.substitute(tag="time")
+    track_point_tag = template.substitute(tag="trkpt")
+    time_tag = template.substitute(tag="time")
     root = xml.etree.ElementTree.parse(filename)
 
     latlon = []
-    for trkpt in root.findall(".//"+trkptTag):
-        latlon.append({"lat": trkpt.get("lat"), "lng": trkpt.get("lon")});
-        
-    for time in root.findall(".//"+timeTag):
-        filenameFromTime = time.text.replace("-", "").replace(":", "")
+    for track_point in root.findall(".//" + track_point_tag):
+        latlon.append({"lat": track_point.get("lat"), "lng": track_point.get("lon")});
+
+    for time in root.findall(".//" + time_tag):
+        filename_from_time = time.text.replace("-", "").replace(":", "")
         break
-    
-    return (filenameFromTime, latlon)
 
-def gpxDataToJsFunc(gpxData):
+    return filename_from_time, latlon
+
+
+def gpx_data_to_js_func(gpxData):
     filename, coordinates = gpxData
-    funcName = "func"+filename
-    
-    jsFnc = "var "+funcName+" = function() {\n";
-    jsFnc += "\tvar coordinates = [\n";
-    
+    func_name = "func" + filename
+
+    js_fnc = "var " + func_name + " = function() {\n"
+    js_fnc += "\tvar coordinates = [\n"
+
     for i, coordinate in enumerate(coordinates):
-        jsFnc += "\t\t{lat: " +coordinate["lat"] + ", lng: " + coordinate["lng"] + "}"
-        if i != len(coordinates)-1:
-            jsFnc += ","
-        jsFnc += "\n";
-        
-    jsFnc += "\t];\n";
-    jsFnc += "\treturn coordinates;\n";
-    jsFnc += "};\n"
+        js_fnc += "\t\t{lat: " + coordinate["lat"] + ", lng: " + coordinate["lng"] + "}"
+        if i != len(coordinates) - 1:
+            js_fnc += ","
+        js_fnc += "\n"
 
-    return (funcName, jsFnc)
+    js_fnc += "\t];\n"
+    js_fnc += "\treturn coordinates;\n"
+    js_fnc += "};\n"
 
-def getAllJsFunc(funcnames):
-    gpsFunc = "var getAllGpsFuncs = function() {\n";
-    gpsFunc += "\tvar funcs = [\n";
+    return func_name, js_fnc
 
-    for i, funcname in enumerate(funcnames):
-        gpsFunc += "\t\t"+funcname
-        if i != len(funcnames)-1:
-            gpsFunc += ","
-        gpsFunc += "\n";
 
-    gpsFunc += "\t];\n";
-    gpsFunc += "\treturn funcs;\n";
-    gpsFunc += "};\n"
+def get_all_js_func(funcnames):
+    gps_func = "var getAllGpsFuncs = function() {\n"
+    gps_func += "\tvar funcs = [\n"
 
-    return gpsFunc
+    for i, func_name in enumerate(funcnames):
+        gps_func += "\t\t" + func_name
+        if i != len(funcnames) - 1:
+            gps_func += ","
+        gps_func += "\n"
 
-def writeJsFile(filename, data):
+    gps_func += "\t];\n"
+    gps_func += "\treturn funcs;\n"
+    gps_func += "};\n"
+
+    return gps_func
+
+
+def write_js_file(filename, data):
     with open(filename, "w") as handle:
         handle.write(data)
 
+
 def main():
-    allJsFuncs = []
-    jsData = ""
-    gpxFiles = getGpxFiles();
-    for i, filename in enumerate(gpxFiles):
-        gpxdata = parseGpx(filename)
-        
-        (funcname, jsFunc) = gpxDataToJsFunc(gpxdata)
-        allJsFuncs.append(funcname)
-        jsData += jsFunc
+    all_js_funcs = []
+    js_data = ""
+    gpx_files = get_gpx_files()
+    for i, filename in enumerate(gpx_files):
+        gpxdata = parse_gpx(filename)
 
-        print(str(i+1) + " of " + str(len(gpxFiles)) + " done")
+        (funcname, jsFunc) = gpx_data_to_js_func(gpxdata)
+        all_js_funcs.append(funcname)
+        js_data += jsFunc
 
-    jsContainerFunc = getAllJsFunc(allJsFuncs)
+        print(str(i + 1) + " of " + str(len(gpx_files)) + " done")
 
-    writeJsFile("../web/backend.js", jsContainerFunc + "\n\n" + jsData)
+    js_container_func = get_all_js_func(all_js_funcs)
+
+    write_js_file("../web/backend.js", js_container_func + "\n\n" + js_data)
+
 
 main()
